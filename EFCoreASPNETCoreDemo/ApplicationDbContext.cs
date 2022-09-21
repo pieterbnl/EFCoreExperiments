0f385e0e859a9a2f14c92e3e3c1dab7ce4eaea3f
Pieter B <pieterbeernink@hotmail.com>
2022-09-21 22:48:38 +0200

    try EF Core in ASP.NET project: create model, dbcontext, connection string and run migrations

---
+++ EFCoreASPNETCoreDemo/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EFCoreASPNETCoreDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreASPNETCoreDemo;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Person> People { get; set; }
+}
---
+++ EFCoreASPNETCoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFCoreASPNETCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
