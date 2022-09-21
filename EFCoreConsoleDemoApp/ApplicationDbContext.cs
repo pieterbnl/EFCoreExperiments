2c42572920217aadd83d49beab3a798d77c98663
Pieter B <pieterbeernink@hotmail.com>
2022-09-21 22:31:30 +0200

    Add project files.

---
+++ EFCoreConsoleDemoApp/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EFCoreConsoleDemoApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreConsoleDemoApp;
+
+internal class ApplicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.;Database=EFCoreConsoleAppDemoDB;Integrated Security=True"); //integrated security=true means Windows credentials will be used        
+    }
+
+    public DbSet<Person> People { get; set; }
+}
