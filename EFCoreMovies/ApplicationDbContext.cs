25177509e9e6e499cb58f3d9b1beabca1dfdaabb
Pieter B <pieterbeernink@hotmail.com>
2022-09-22 21:28:48 +0200

    try limiting field length and requiring a field by annotations and fluentapi, try rerun migrations and see the difference

--- EFCoreMovies/ApplicationDbContext.cs
+++ EFCoreMovies/ApplicationDbContext.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCoreMovies.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace EFCoreMovies
+namespace EFCoreMovies;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext(DbContextOptions options) : base(options)
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-        }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // modelBuilder.Entity<Genre>().HasKey(p => p.Id); // for demonstration purposes only, not needed when following EF conventions
+        modelBuilder.Entity<Genre>()
+            .Property(p => p.Name).HasMaxLength(150)
+            .IsRequired(); 
+    }
+
+    public DbSet<Genre> Genres { get; set; }
 }
---
+++ EFCoreMovies/Entities/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreMovies.Entities;
+
+public class Genre
+{
+    public int Id { get; set; }
+    
+    // [StringLength(maximumLength: 150)] // example of limiting property max length
+    public string Name { get; set; }
+}
