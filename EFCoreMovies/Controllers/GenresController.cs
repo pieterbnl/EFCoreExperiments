4f2bd1e2d6d843980ffea1dd3298a1805731c1a3
Pieter B <pieterbeernink@hotmail.com>
2022-09-26 11:37:27 +0200

    selecting specific records with FirstOrDefaultAsync and Where

--- EFCoreMovies/Controllers/GenresController.cs
+++ EFCoreMovies/Controllers/GenresController.cs
@@ -21,4 +21,32 @@ public class GenresController : ControllerBase
     {
         return await _context.Genres.AsNoTracking().ToListAsync();
     }
+
+
+
+    /* for demo purposes only - will return a 500 error if no records are found
+    [HttpGet("first")]
+    public async Task<Genre> GetFirst()
+    {
+        return await _context.Genres.FirstAsync(g => g.Name.Contains("p"));
+    }*/    
+
+    [HttpGet("first")]
+    public async Task<ActionResult<Genre>> GetFirst()
+    {        
+        var genre = await _context.Genres.AsNoTracking().FirstOrDefaultAsync(g => g.Name.Contains("z"));
+        
+        if (genre is null)
+        {
+            return NotFound(); // returns a 404
+        }
+        return genre;
+    }
+
+    [HttpGet("filter")]
+    public async Task<IEnumerable<Genre>> Filter(string name)
+    {
+        return await _context.Genres.Where(g => g.Name.Contains(name)).ToListAsync();
+        //return await _context.Genres.Where(g => g.Name.StartsWith(name)).ToListAsync();
+    }
 }
