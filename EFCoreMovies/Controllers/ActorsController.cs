4b98b4675a21d6cb615c23be6dbe7f8581096a2c
Pieter B <pieterbeernink@hotmail.com>
2022-09-30 14:25:57 +0200

    try updating (Actors) with the EF 'disconnected model'

--- EFCoreMovies/Controllers/ActorsController.cs
+++ EFCoreMovies/Controllers/ActorsController.cs
@@ -40,8 +40,9 @@ public class ActorsController : ControllerBase
         return Ok();
     }
 
+    // Example of updating with connected model
     [HttpPut("{id}")]
-    public async Task<ActionResult> Put(ActorCreationDTO actorCreationDTO, int id)
+    public async Task<ActionResult> PutConnected(ActorCreationDTO actorCreationDTO, int id)
     {
         var actorDB = await _context.Actors
             .FirstOrDefaultAsync(p => p.Id == id);
@@ -56,4 +57,30 @@ public class ActorsController : ControllerBase
         await _context.SaveChangesAsync();
         return Ok();            
     }
+
+
+    // Example of updating with disconnected model
+    // Updating with the ‘disconnected model’ comprises of updating the entity
+    // without having the dbcontext previously loaded that entity.
+    [HttpPut("disconnected/{id:int}")]
+    public async Task<ActionResult> PutDisconnected(ActorCreationDTO actorCreationDTO, int id)
+    {
+        // Check if actor exists
+        var existsActor = await _context.Actors.AnyAsync(a => a.Id == id);
+        if (!existsActor) return NotFound();
+
+        var actor = _mapper.Map<Actor>(actorCreationDTO);
+        actor.Id = id;
+
+        // In order to do the update using the disconnected model, the status of the entity is used.
+        // Note: using the .Update like this, will result in a (SQL) query
+        // that updates all of the properties (read: columns) in the table.
+        // No matter if there where actually changes made to those properties.
+        // Reason being: the entity itself gets marked as changed, meaning that when executing SaveChanges,
+        // EF will end up updating every single column that belongs to that entity.
+        _context.Update(actor);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+    
 }
