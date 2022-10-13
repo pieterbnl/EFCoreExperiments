021f246073e5b4dbc203aacb90a6ca18cfc00958
Pieter B <pieterbeernink@hotmail.com>
2022-10-13 22:12:39 +0200

    apply a value conversion to CinemaHallType

--- EFCoreMovies/Entities/Configurations/CinemaHallConfig.cs
+++ EFCoreMovies/Entities/Configurations/CinemaHallConfig.cs
@@ -8,6 +8,7 @@ public class CinemaHallConfig : IEntityTypeConfiguration<CinemaHall>
     public void Configure(EntityTypeBuilder<CinemaHall> builder)
     {
         builder.Property(p => p.Cost).HasPrecision(precision: 9, scale: 2);
-        builder.Property(p => p.CinemaHallType).HasDefaultValue(CinemaHallType.TwoDimensions);
+        builder.Property(p => p.CinemaHallType).HasDefaultValue(CinemaHallType.TwoDimensions)
+            .HasConversion<string>();
     }
 }
